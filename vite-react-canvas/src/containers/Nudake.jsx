515311c4d1cc7523a91671e73eebb477b9f877c8
이원종 <lwj@iwonjong-ui-MacBookPro.local>
2023-11-05 18:51:14 +0900

    - 이미지 지우는거까지

---
+++ vite-react-canvas/src/containers/Nudake.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef } from 'react'
+import '../style/containers/Nudake.css'
+
+import image1 from '../assets/nudake-1.jpg'
+import image2 from '../assets/nudake-2.jpg'
+import image3 from '../assets/nudake-3.jpg'
+import { getAngle, getDistance } from '../utils/utils'
+
+const Nudake = () => {
+    const canvasRef = useRef(null)
+    useEffect(() => {
+        const canvas = canvasRef.current
+        const canvasParent = canvas.parentNode
+        const ctx = canvas.getContext('2d')
+        let canvasWidth, canvasHeight
+
+        const imageSrcs = [image1, image2, image3]
+        let currIndex = 0
+        let prevPos = { x: 0, y: 0 }
+
+        function resize() {
+            canvasWidth = canvasParent.clientWidth
+            canvasHeight = canvasParent.clientHeight
+            canvas.style.width = canvasWidth + 'px'
+            canvas.style.height = canvasHeight + 'px'
+            canvas.width = canvasWidth
+            canvas.height = canvasHeight
+
+            drawImage()
+        }
+
+        function drawImage() {
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight)
+            const image = new Image()
+            image.src = imageSrcs[currIndex]
+            image.onload = () => {
+                ctx.drawImage(image, 0, 0, canvasWidth, canvasHeight)
+            }
+        }
+
+        const onMousedown = (e) => {
+            canvas.addEventListener('mouseup', onMouseUp)
+            canvas.addEventListener('mouseleave', onMouseUp)
+            canvas.addEventListener('mousemove', onMouseMove)
+            prevPos = { x: e.offsetX, y: e.offsetY }
+        }
+        const onMouseUp = () => {
+            canvas.removeEventListener('mouseup', onMouseUp)
+            canvas.removeEventListener('mouseleave', onMouseUp)
+            canvas.removeEventListener('mousemove', onMouseMove)
+        }
+        const onMouseMove = (e) => {
+            drawCircles(e)
+        }
+
+        function drawCircles(e) {
+            const nextPos = { x: e.offsetX, y: e.offsetY }
+            const dist = getDistance(prevPos, nextPos)
+            const angle = getAngle(prevPos, nextPos)
+            for (let i = 0; i < dist; i++) {
+                const x = prevPos.x + Math.cos(angle) * i
+                const y = prevPos.y + Math.sin(angle) * i
+                ctx.globalCompositeOperation = 'destination-out'
+                ctx.beginPath()
+                ctx.arc(x, y, canvasWidth / 30, 0, Math.PI)
+                ctx.fill()
+                ctx.closePath()
+            }
+
+            prevPos = nextPos
+        }
+
+        canvas.addEventListener('mousedown', onMousedown)
+        window.addEventListener('resize', resize)
+        resize()
+
+        return () => {
+            window.removeEventListener('resize', resize)
+        }
+    }, [])
+
+    return (
+        <div className="nudake">
+            <canvas ref={canvasRef} />
+        </div>
+    )
+}
+
+export default Nudake
